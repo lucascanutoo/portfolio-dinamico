a415c96816a289f49834804ebaf1f608cd5c42b8
lucascanutoo <lucascanutode@gmail.com>
2026-02-19 09:24:16 -0300

    correção main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from app.db.database import engine, Base
 from app.routers.projetos import router
+from app.routers import projetos, perfil
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 app.include_router(router, prefix="/api")
+app.include_router(projetos.router, prefix="/api")
+app.include_router(perfil.router, prefix="/api")
 
 @app.get("/")
 def read_root():
